291f04afefcfde62129b46bf81e1456ee80e8f36
CONURE-SAQEEF <muhammad.shabbir@conurets.com>
2022-08-31 13:24:49 +0500

    Initial Commit

---
+++ src/test/java/com/conure/restaurant_application_backend_v1/RestaurantApplicationBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conure.restaurant_application_backend_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationBackendV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
