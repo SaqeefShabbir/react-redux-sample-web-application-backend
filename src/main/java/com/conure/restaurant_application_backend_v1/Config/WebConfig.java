17b59aea664b7f64462f13c0241cb1e94361e1c8
Saqeef Shabbir <62050152+SaqeefShabbir@users.noreply.github.com>
2022-11-12 21:50:06 +0500

    Update WebConfig.java

--- src/main/java/com/conure/restaurant_application_backend_v1/Config/WebConfig.java
+++ src/main/java/com/conure/restaurant_application_backend_v1/Config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        //registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/api/**").allowedOrigins("https://mywebapp00002.azurewebsites.net");
     }
 }
